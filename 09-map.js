34e1eee4406be8888de10a361fe711e74218ef10
Pedro Bim <pedrodinizz2@outlook.com>
2024-04-23 19:57:20 -0300

    map

---
+++ 09-map.js
@@ -0,0 +1,27 @@
+const pessoas = [
+  {id: 3, nome: 'Luiz'},
+  {id: 2, nome: 'Maria'},
+  {id: 1, nome: 'Helena'},
+];
+
+// const novasPessoas = {};
+// for (const pessoa of pessoas) {
+//   const { id } = pessoa;
+//    novasPessoas[id] = { ...pessoa }; 
+// };
+
+const novasPessoas = new Map();
+for (const pessoa of pessoas) {
+  const { id } = pessoa;
+   novasPessoas.set(id, { ...pessoa });
+};
+
+// console.log(novasPessoas);
+// console.log(novasPessoas.get(2));
+
+for(const pessoa of novasPessoas) {
+  // console.log(pessoa);
+}
+
+novasPessoas.delete(2);
+console.log(novasPessoas);
